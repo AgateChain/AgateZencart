34c8a0cfc80701a21be7ebc7dadf184050a8af5f
R. Giridhar <gdhar67@gmail.com>
2018-06-08 19:18:09 +0530

    Adding language

---
+++ src/includes/languages/english/modules/payment/agate.php
@@ -0,0 +1,5 @@
+<?php
+
+define('MODULE_PAYMENT_AGATE_TEXT_TITLE', 'Agate');
+define('MODULE_PAYMENT_AGATE_TEXT_DESCRIPTION', 'Accept Payments with Agate.');
+define('MODULE_PAYMENT_AGATE_TEXT_CHECKOUT', 'Pay With Agate');
